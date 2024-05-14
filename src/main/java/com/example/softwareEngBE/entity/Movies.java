99223f3e257fbb97f59ff9baea1dc68e2ec23997
jiyoxn <jiyoxn03@naver.com>
2024-05-14 20:47:48 +0900

    ADD :  Movies에 rating추가(주석처리함)

--- src/main/java/com/example/softwareEngBE/entity/Movies.java
+++ src/main/java/com/example/softwareEngBE/entity/Movies.java
@@ -1,5 +1,6 @@
 package com.example.softwareEngBE.entity;
 
+import com.example.softwareEngBE.dto.CommentDto;
 import com.example.softwareEngBE.dto.MoviesDto;
 import jakarta.persistence.*;
 import lombok.*;
@@ -18,8 +19,15 @@ public class Movies {
     private String title;
     @Column
     private String genres;
+//    @JoinColumn(name = "rating")
+//    private Float rating; // 영화 평점 필드 추가
 
-    public static Movies createMovies(MoviesDto dto){
-        return new Movies(dto.getMovie_Id(), dto.getTitle(), dto.getGenres());
+    public static Movies createMovies(MoviesDto dto, CommentDto commentDto){
+        return new Movies(
+                dto.getMovie_Id(),
+                dto.getTitle(),
+                dto.getGenres()
+//                commentDto.getRating()
+        );
     }
 }
