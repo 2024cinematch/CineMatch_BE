b66089bd48666fadff1a50aa59c67e58a5342f0e
jiyoxn <jiyoxn03@naver.com>
2024-05-14 19:47:03 +0900

    DEL : MoviesSearchController import 수정

--- src/main/java/com/example/softwareEngBE/controller/MoviesSearchController.java
+++ src/main/java/com/example/softwareEngBE/controller/MoviesSearchController.java
@@ -1,14 +1,12 @@
 package com.example.softwareEngBE.controller;
 
 import com.example.softwareEngBE.dto.MoviesDto;
-import com.example.softwareEngBE.entity.Movies;
 import com.example.softwareEngBE.service.MoviesSearchService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
