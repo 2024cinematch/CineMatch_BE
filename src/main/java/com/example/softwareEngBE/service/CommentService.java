6e3f6af1340231dd97e389948cdc9ce3221764da
jiyoxn <jiyoxn03@naver.com>
2024-05-28 16:38:31 +0900

    MOD: comments 수정

--- src/main/java/com/example/softwareEngBE/service/CommentService.java
+++ src/main/java/com/example/softwareEngBE/service/CommentService.java
@@ -11,41 +11,36 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Slf4j
 @Service
 public class CommentService {
+
     @Autowired
     CommentRepository commentRepository;
+
     @Autowired
     MoviesRepository moviesRepository;
+
     @Autowired MoviesSearchService moviesSearchService;
 
     public List<CommentDto> comments(String title) {
-        int id= moviesSearchService.findByTitletoId(title);
-        List<Comment> comments=commentRepository.findByMovieId(id);
-
-        List<CommentDto> commentDtos=new ArrayList<CommentDto>();
-        for(int i=0;i<comments.size();i++){
-            Comment c=comments.get(i);
-            CommentDto dto=CommentDto.createCommentDto(c);
+        List<Comment> comments = commentRepository.findByMovieTitleIgnoreCase(title);
+        List<CommentDto> commentDtos = new ArrayList<>();
+        for (Comment c : comments) {
+            CommentDto dto = CommentDto.createCommentDto(c);
             commentDtos.add(dto);
         }
         return commentDtos;
     }
 
-
 //생성
     public CommentDto create(int id, CommentDto dto) {
     Movies movies = moviesRepository.findById(id)
             .orElseThrow(()-> new IllegalArgumentException("No exist movie"));
-
-
-
     //댓글 엔티티 생성
     Comment comment=Comment.createComment(dto,movies);
-
     Comment created=commentRepository.save(comment);
     return CommentDto.createCommentDto(created);
     }
-
 }
